c6d4f17742361605fe18e36229c371cb3bc76617
KeramKeram <rev@autograf.pl>
2022-07-24 22:56:35 +0200

    json: Add empty function who is adding rect.

--- jsonfunctions.py
+++ jsonfunctions.py
@@ -6,3 +6,7 @@ def load_json(path):
     data = json.load(f)
     f.close()
     return data
+
+
+def add_rect_to_json(coordinates, class_name, json_data):
+    return json_data
--- testjsonfunctions.py
+++ testjsonfunctions.py
@@ -1,9 +1,23 @@
+from collections import namedtuple
+import json
+import jsonfunctions
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    # Test type: Approval test
+    def test_json_generation(self):
+        golden_json_file = open('approvaltestdata/json_example.json')
+        data_golden = json.load(golden_json_file)
+        json_file_example = open('templatejson.json')
+        data_example = json.load(json_file_example)
+
+        Points = namedtuple('Points', ['x1', 'y1', 'x2', 'y2'])
+        first_rect = Points(579, 584, 924, 1120)
+        jason_output = jsonfunctions.add_rect_to_json(first_rect, "class1", data_example)
+        second_rect = Points(120, 400, 1150, 800)
+        jason_output = jsonfunctions.add_rect_to_json(second_rect, "class1", jason_output)
+        self.assertEqual(data_golden, jason_output)  # add assertion here
 
 
 if __name__ == '__main__':
