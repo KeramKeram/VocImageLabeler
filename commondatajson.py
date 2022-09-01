be76c914ba9e9c644cff9cf25893a0dcc62d3f95
KeramKeram <rev@autograf.pl>
2022-09-01 22:25:13 +0200

    json: add function for init json file

---
+++ commondatajson.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+
+CommonJsonData = namedtuple('CommonJsonData', ['folder', 'filename', 'path', 'width', 'height'])
--- jsonfunctions.py
+++ jsonfunctions.py
@@ -1,3 +1,4 @@
+from commondatajson import CommonJsonData
 import json
 
 
@@ -10,11 +11,11 @@ def load_json(path):
 
 def add_rect_to_json(coordinates, class_name, json_data):
     data = {"name": str(class_name),
-                        "pose": "Unspecified",
-                        "truncated": 0,
-                        "difficult": 0,
-                        "bndbox": {"xmin": coordinates.x1, "ymin": coordinates.y1, "xmax": coordinates.x2,
-                                   "ymax": coordinates.y2}}
+            "pose": "Unspecified",
+            "truncated": 0,
+            "difficult": 0,
+            "bndbox": {"xmin": coordinates.x1, "ymin": coordinates.y1, "xmax": coordinates.x2,
+                       "ymax": coordinates.y2}}
     current_data = json_data['data'][0]['annotation']
     if 'object' not in current_data:
         current_data['object'] = []
@@ -22,3 +23,16 @@ def add_rect_to_json(coordinates, class_name, json_data):
 
     json_data['data'][0]['annotation'] = current_data
     return json_data
+
+
+def prepare_json_file(commn_data):
+    json_file_template = open('testtemplatejson.json')
+    data_template = json.load(json_file_template)
+    current_data = data_template['data'][0]['annotation']
+    current_data['folder'] = str(commn_data.folder)
+    current_data['filename'] = str(commn_data.filename)
+    current_data['path'] = str(str(commn_data.path) + "/" + commn_data.filename)
+    current_data['size']['width'] = commn_data.width
+    current_data['size']['height'] = commn_data.height
+    data_template['data'][0]['annotation'] = current_data
+    return data_template
