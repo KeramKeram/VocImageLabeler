ecb2c0f0861fe1ffd5dc4b7149971fd4bebe82ec
KeramKeram <rev@autograf.pl>
2022-09-01 21:23:04 +0200

    main: Remove semigraphic ui and use standard in instead.

--- main.py
+++ main.py
@@ -1,44 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import detectorjetson
+from collections import namedtuple
 from os import listdir
 from os.path import isfile, join
-import jetson.utils
-import npyscreen
+#import detectorjetson
+#import jetson.utils
 
 
-class TestApp(npyscreen.NPSApp):
-    def __init__(self):
-        self.path_to_model = None
-        self.path_to_model_label = None
-        self.path_to_labels = None
-        self.path_to_labels_label = None
-        self.path_to_images = None
-        self.path_to_images_label = None
+def main():
+    paths_tuple = namedtuple('paths', ['path_to_images', 'path_to_images_label', 'path_to_model'])
+    paths_tuple.path_to_images = input("Path to images:")
+    paths_tuple.path_to_images_label = input("Path to labels:")
+    paths_tuple.path_to_model = input("Path to model:")
 
-    def main(self):
-        main_screen = npyscreen.Form(name="VocImageLabeler", )
-        self.path_to_images_label = main_screen.add(npyscreen.TitleFixedText, name="Path to images:")
-        self.path_to_images = main_screen.add(npyscreen.Textfield, value="type here")
-        self.path_to_labels_label = main_screen.add(npyscreen.TitleFixedText, name="Path to labels:")
-        self.path_to_labels = main_screen.add(npyscreen.Textfield, value="type here")
-        self.path_to_model_label = main_screen.add(npyscreen.TitleFixedText, name="Path to model:")
-        self.path_to_model = main_screen.add(npyscreen.Textfield, value="type here")
-        self.path_to_images_label = main_screen.add(npyscreen.TitleFixedText, name="To start click button")
-        start_button = main_screen.add(npyscreen.Button, name="Start", value_changed_callback=self.start)
-        # This lets the user interact with the Form.
-        main_screen.edit()
 
-    def start(self, widget):
-        contents = listdir(str(self.path_to_images.value))
-        files = filter(lambda f: isfile(join(self.path_to_images.value,f)),contents)
-        files_list = list(files)
-        detector = detectorjetson.DetectorJetson(32, 32, str(self.path_to_model.value),
-                                                 str(self.path_to_model_label.value))
-        for file in files_list:
-            image = jetson.utils.loadImage(str(self.path_to_images_label.value) + "/" + file)
-            rect_list = detector.run(image)
+def start(widget):
+    contents = listdir(str(self.path_to_images.value))
+    files = filter(lambda f: isfile(join(self.path_to_images.value, f)), contents)
+    files_list = list(files)
+    detector = detectorjetson.DetectorJetson(32, 32, str(self.path_to_model.value),
+                                             str(self.path_to_model_label.value))
+    for file in files_list:
+        image = jetson.utils.loadImage(str(self.path_to_images_label.value) + "/" + file)
+        rect_list = detector.run(image)
 
 if __name__ == "__main__":
-    App = TestApp()
-    App.run()
+    main()
