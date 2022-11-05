a8323d91ff5e1f5270a999b923e9572f013bc324
KeramKeram <rev@autograf.pl>
2022-11-05 23:03:38 +0100

    Refactor: Remove not used file.

--- factory.py
+++
@@ -1,9 +0,0 @@
-import detectorjetson
-import detectorpytrochssd
-
-
-def factory(type, paths_tuple):
-    if type == 1:
-        return detectorjetson.DetectorJetson(paths_tuple.path_to_model, paths_tuple.path_to_images_label)
-    else:
-        return detectorpytrochssd.DetectorPytrochSSD(paths_tuple.path_to_model, paths_tuple.path_to_images_label)
